f90605bf7147e1b2ab67474c135c3b58e67f51b2
princemahesh <princejagdishrohara@gmail.com>
2023-01-07 10:45:57 +0530

    transation ui chnage

--- src/pages/customers/Customers.jsx
+++ src/pages/customers/Customers.jsx
@@ -3,6 +3,7 @@ import FetchDataComponent from "../../components/fetchdata/FetchDataComponent";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import BookingComponent from "../bookings/Booking.component";
+import CustomerList from "./CustomerList.component";
 const Customers = () => {
   return (
     <div className="single ">
@@ -13,7 +14,7 @@ const Customers = () => {
           <h1 className="mt-8 ml-4 text-3xl mb-4">Customers</h1>
         </div>
 
-        <BookingComponent />
+        <CustomerList />
       </div>
     </div>
   );
--- src/pages/transations/Transation.jsx
+++ src/pages/transations/Transation.jsx
@@ -3,6 +3,7 @@ import FetchDataComponent from "../../components/fetchdata/FetchDataComponent";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import BookingComponent from "../bookings/Booking.component";
+import TransationsComponent from "./Transations.component";
 const Transation = () => {
   return (
     <div className="single ">
@@ -10,10 +11,10 @@ const Transation = () => {
       <div className="singleContainer p-4 ml-4 mr-4">
         <Navbar />
         <div className="">
-          <h1 className="mt-8 ml-4 text-3xl mb-4">Transation</h1>
+          <h1 className="mt-8 ml-4 text-3xl mb-4">Transations</h1>
         </div>
 
-        <BookingComponent />
+        <TransationsComponent />
       </div>
     </div>
   );
