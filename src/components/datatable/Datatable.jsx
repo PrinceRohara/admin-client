756940d6eb98b3f3c5b4b3bd6454ad7d43a36da0
princemahesh <princejagdishrohara@gmail.com>
2023-01-07 11:44:04 +0530

    salons ui

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -3,14 +3,34 @@ import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../datatablesource";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-
+import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
 const Datatable = () => {
   const [data, setData] = useState(userRows);
 
-  const handleDelete = (id) => {
-    setData(data.filter((item) => item.id !== id));
-  };
+  // const handleDelete = (id) => {
+  //   setData(data.filter((item) => item.id !== id));
+  // };
 
+  const names = [
+    {
+      value: 545,
+      label: "All",
+    },
+    {
+      value: 54325,
+      label: "Mahesh",
+    },
+    {
+      value: 5435,
+      label: "Vincenzo",
+    },
+    {
+      value: 5245,
+      label: "Tom ",
+    },
+  ];
   const actionColumn = [
     {
       field: "action",
@@ -22,12 +42,6 @@ const Datatable = () => {
             <Link to="/users/test" style={{ textDecoration: "none" }}>
               <div className="viewButton">View</div>
             </Link>
-            <div
-              className="deleteButton"
-              onClick={() => handleDelete(params.row.id)}
-            >
-              Delete
-            </div>
           </div>
         );
       },
@@ -37,12 +51,47 @@ const Datatable = () => {
     <div className="datatable">
       <div className="datatableTitle">
         Salons
-        <Link
-          to="/users/new"
-          className=" bg-[#D1461B] text-lg font-bold p-2  text-white rounded"
-        >
-          CREATE SALON
-        </Link>
+        <div className="flex space-x-8 mt-2 mb-4">
+          <TextField
+            className="p-4  w-52 border-none float-right ml-48 "
+            id="standard-select-currency"
+            select
+            label="Select Owner"
+            defaultValue="All"
+            defaultChecked
+            variant="filled"
+            size="small"
+            color="warning"
+          >
+            {names.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <Link to="/users/new" className="  text-white rounded ">
+            {/* bg-[#D1461B] text-lg font-bold p-1  */}
+            <Button color="error" variant="contained" size="large">
+              Create Salon
+            </Button>
+          </Link>
+        </div>
+      </div>{" "}
+      <div className=" p-2 ">
+        <button className="text-md p-2 my-1 border-orange-500  tracking-wide border-b-[3px]  text-gray-700  cursor-pointer  ">
+          All Salons
+        </button>
+      </div>
+      <hr />
+      <div className="my-2">
+        <TextField
+          id="outlined-basic"
+          className="p-2 ml-2 w-[100%]"
+          variant="outlined"
+          classes=""
+          size="small"
+          color="warning"
+        />
       </div>
       <DataGrid
         className="datagrid"
