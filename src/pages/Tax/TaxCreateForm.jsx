e93d8cc26d27cbf11c19053c5e971957aa86bf85
princemahesh <princejagdishrohara@gmail.com>
2023-01-15 19:11:37 +0530

    adding form

--- src/pages/Tax/TaxCreateForm.jsx
+++ src/pages/Tax/TaxCreateForm.jsx
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
+import axios from "axios";
 const TaxCreateForm = () => {
   const taxtype = ["GST-12", "GST-17", "GST-18", "Service Tax", "Others"];
+
+  const defaultFields = {
+    tax_name: "",
+    tax_rate: "",
+    tax_type: "",
+    tax_description: "",
+  };
+  const [form, setForm] = useState(defaultFields);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => {
+      return { ...prev, [name]: value };
+    });
+    // console.log(e.target);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("rrun");
+    try {
+      // const res = await axios({
+      //   method: "POST",
+      //   url: "https://spaalon.harij.in/api/backend/AddTax",
+
+      //   headers: { "Content-Type": "application/json" },
+      //   body: JSON.stringify(form),
+      // });
+
+      const res = await axios.post(
+        "https://spaalon.harij.in/api/backend/AddTax",
+        JSON.stringify(form)
+      );
+      console.log(res);
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setForm(defaultFields);
+        alert("success");
+      } else {
+        alert("Some error occured");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    // console.log(form, "main form");
+  };
+
+  console.log(form, "myform");
   return (
     <div>
-      <form className="p-2 m-2 bg-white rounded ">
+      <form onSubmit={handleSubmit} className="p-2 m-2 bg-white rounded ">
         <h3 className="text-xl  font-bold ml-4 mt-4">General Information</h3>
         <div className="p-4 m-2 flex space-x-4 hover:border-orange-300">
           <TextField
@@ -14,6 +61,8 @@ const TaxCreateForm = () => {
             label="Tax Name"
             variant="outlined"
             color="warning"
+            onChange={handleChange}
+            name="tax_name"
           />
           <TextField
             className="w-[50%] p-1 ml-2"
@@ -21,6 +70,8 @@ const TaxCreateForm = () => {
             label="Tax Rate"
             variant="outlined"
             color="warning"
+            onChange={handleChange}
+            name="tax_rate"
           />
         </div>
 
@@ -33,6 +84,8 @@ const TaxCreateForm = () => {
             defaultValue="EUR"
             color="warning"
             variant="outlined"
+            onChange={handleChange}
+            name="tax_type"
           >
             {taxtype.map((option) => (
               <MenuItem value={option}>{option}</MenuItem>
@@ -47,17 +100,25 @@ const TaxCreateForm = () => {
             label="Description"
             variant="outlined"
             color="warning"
+            onChange={handleChange}
+            name="tax_description"
           />
         </div>
+        <button
+          type="submit"
+          className=" float-right p-2 m-2  bg-orange-700 opacity-20 uppercase text-white rounded "
+        >
+          Save
+        </button>
       </form>
       <div className="mt-36 ">
-        <div className="fixed ml-[65rem]">
-          <button
-            disabled
+        <div className=" ml-[65rem]">
+          {/* <button
+            type="submit"
             className=" float-right p-2 m-2  bg-orange-700 opacity-20 uppercase text-white rounded "
           >
             Save
-          </button>
+          </button> */}
           <button className="text-gray-500 float-right p-2 m-2  ">Back</button>
         </div>
       </div>
--- src/pages/owners/OwnerForm.jsx
+++ src/pages/owners/OwnerForm.jsx
@@ -1,11 +1,57 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
 import "../../style/dark.scss";
-
+import { useState } from "react";
+import axios from "axios";
 const OwnerForm = () => {
+  const defaultFields = {
+    first_name: "",
+    last_name: "",
+    contact_no: "",
+    email: "",
+    password: "",
+    note: "",
+  };
+
+  const [form, setForm] = useState(defaultFields);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => {
+      return { ...prev, [name]: value };
+    });
+    // console.log(e.target);
+  };
+  console.log(form, "form page ");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("rrun");
+    try {
+      const res = await axios({
+        method: "POST",
+        url: "https://spaalon.harij.in/api/backend/AddOwner",
+
+        headers: { "Content-Type": "application/json" },
+        body: { data: JSON.stringify(form) },
+      });
+
+      console.log(res);
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setForm(defaultFields);
+        alert("success");
+      } else {
+        alert("Some error occured");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    // console.log(form, "main form");
+  };
+
+  console.log(form, "myform");
   return (
     <>
-      <form className="p-2 m-2">
+      <form onSubmit={handleSubmit} className="p-2 m-2">
         <h3 className="text-xl  font-bold ml-4 mt-4">General Information</h3>
         <div className="p-4 m-2 flex space-x-4 hover:border-orange-300">
           <TextField
@@ -14,6 +60,8 @@ const OwnerForm = () => {
             label="First Name"
             variant="outlined"
             color="warning"
+            onChange={handleChange}
+            name="first_name"
           />
         </div>
         <div className="p-4 m-2 flex space-x-4 hover:border-orange-300">
@@ -23,6 +71,8 @@ const OwnerForm = () => {
             label="Last Name"
             variant="outlined"
             color="warning"
+            onChange={handleChange}
+            name="last_name"
           />
         </div>
         <div className="p-4 m-2 flex space-x-4 hover:border-orange-300">
@@ -32,6 +82,8 @@ const OwnerForm = () => {
             label="Contact Number"
             variant="outlined"
             color="warning"
+            onChange={handleChange}
+            name="contact_no"
           />
         </div>
         <div className="p-4 m-2 flex space-x-4 hover:border-orange-300">
@@ -41,6 +93,8 @@ const OwnerForm = () => {
             label="E-mail Address"
             variant="outlined"
             color="warning"
+            onChange={handleChange}
+            name="email"
           />
         </div>{" "}
         <div className="p-4 m-2 flex space-x-4 hover:border-orange-300">
@@ -51,6 +105,8 @@ const OwnerForm = () => {
             variant="outlined"
             color="warning"
             type="password"
+            name="password"
+            onChange={handleChange}
           />
         </div>{" "}
         <div className="p-4 m-2 flex space-x-4 hover:border-orange-300">
@@ -62,8 +118,11 @@ const OwnerForm = () => {
             rows={2}
             variant="outlined"
             color="warning"
+            onChange={handleChange}
+            name="note"
           />
         </div>
+        <button type="submit">send</button>
       </form>
       <button className="bg-orange-700 p-2 m-4 rounded text-white float-right">
         Save
